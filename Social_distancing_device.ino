283275925a5ba7696354763b2410c4fdfcb6281f
Nikhil Mhatre <nikhilamhatre18@gmail.com>
2020-09-02 18:38:50 -0400

    Update Social_distancing_device.ino

--- Social_distancing_device.ino
+++ Social_distancing_device.ino
@@ -1,3 +1,7 @@
+/*This poject was created in light of the Coronavirus pandemic.
+The code below programs a wearable Arduino social distancing device that will alert the user in a highly populated when a person in within 6 feet of them. 
+This would help both the user and others to practice social distancing in order to lower the infection rate. 
+*/
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int trigPin = 8;
@@ -17,14 +21,14 @@ void setup(){
 
 void loop()
 {
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, LOW);           //programming the ultrasonic sensor signal
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
+  distance = (duration*.0343)/2;        //utilizing the time differnece in the signal to calculated the distance between people
   distance = distance/30.48;
   Serial.print("Distance: ");
   Serial.println(distance);
@@ -35,7 +39,7 @@ void loop()
 
   if (distance<6)
   {
-    digitalWrite(7, LOW);
+    digitalWrite(7, LOW);             //monitoring the distance reorded by the sensor and utilizing a conditional statement to activate proximity buzzer  
     digitalWrite(6, HIGH);
     tone(buzzer, 1000);
     delay(50); 
